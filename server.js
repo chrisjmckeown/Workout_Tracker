82e778f909fde227f9ee63d8d575260fff7d25b9
Chris Mckeown <chris.j.mckeown@hotmail.com>
2020-11-28 11:59:35 +1100

    mongoAtlas db connection added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,17 +14,21 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
-
 // routes
 app.use(require("./routes/api.js"));
 app.use(require("./routes/html.js"));
 
+// Connect to Mongo
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("MongoDB Connected...");
 
-app.listen(PORT, () => {
-
-  console.log("App running on: http://localhost:" + PORT);
-});
+    app.listen(PORT, () => {
+      console.log("App running on: http://localhost:" + PORT);
+    });
+  })
+  .catch((err) => console.log(err));
